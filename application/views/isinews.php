28c8830599d9fddcc7e8210cd51dc2c48b4ec014
aditt27 <adittbudi@gmail.com>
2018-05-12 11:40:53 +0700

    update ui news

--- application/views/isinews.php
+++ application/views/isinews.php
@@ -1,136 +1,95 @@
 <!-- Page Content -->
-<div style="margin-top: 55px;" class="container">
+<div style="margin-top: 75px;" class="container">
+
+	<!-- Hot Blog Post -->
+	<div style="position: relative">
+		<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
+		<div style="position: absolute; bottom: 0px; left: 0px; padding: 20px; width: 100%; height: max-content; max-height: 100%; background:#7f7f7f; background:rgba(255,255,255,0.5);">
+			<h2>
+				<a href="#">Hot Post Title</a>
+			</h2>
+			<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author</p>
+			<p> August 28, 2013 | <span class="glyphicon glyphicon-eye-open"></span> 0</p>
+		</div>
+	</div>
+
+	<hr>
 
 	<div class="row">
 
 		<!-- Blog Entries Column -->
-		<div class="col-md-8">
-
-			<h1 class="page-header">
-				Page Heading
-				<small>Secondary Text</small>
-			</h1>
-
-			<!-- First Blog Post -->
-			<h2>
-				<a href="#">Blog Post Title</a>
-			</h2>
-			<p class="lead">
-				by <a href="index.php">Start Bootstrap</a>
-			</p>
-			<p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-			<hr>
-			<img class="img-responsive" src="http://placehold.it/900x300" alt="">
-			<hr>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-			<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-			<hr>
-
-			<!-- Second Blog Post -->
-			<h2>
-				<a href="#">Blog Post Title</a>
-			</h2>
-			<p class="lead">
-				by <a href="index.php">Start Bootstrap</a>
-			</p>
-			<p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-			<hr>
-			<img class="img-responsive" src="http://placehold.it/900x300" alt="">
-			<hr>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-			<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-			<hr>
-
-			<!-- Third Blog Post -->
-			<h2>
-				<a href="#">Blog Post Title</a>
-			</h2>
-			<p class="lead">
-				by <a href="index.php">Start Bootstrap</a>
-			</p>
-			<p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-			<hr>
-			<img class="img-responsive" src="http://placehold.it/900x300" alt="">
-			<hr>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-			<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-			<hr>
-
-			<!-- Pager -->
-			<ul class="pager">
-				<li class="previous">
-					<a href="#">&larr; Older</a>
-				</li>
-				<li class="next">
-					<a href="#">Newer &rarr;</a>
-				</li>
-			</ul>
-
-		</div>
-
-		<!-- Blog Sidebar Widgets Column -->
 		<div class="col-md-4">
 
-			<!-- Blog Search Well -->
-			<div class="well">
-				<h4>Blog Search</h4>
-				<div class="input-group">
-					<input type="text" class="form-control">
-					<span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
+			<!--Blog Post -->
+			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
+			<br>
+			<h2>
+				<a>Blog Post Title</a>
+			</h2>
+			<div class="row">
+				<div class="col-md-10">
+					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
 				</div>
-				<!-- /.input-group -->
-			</div>
 
-			<!-- Blog Categories Well -->
-			<div class="well">
-				<h4>Blog Categories</h4>
-				<div class="row">
-					<div class="col-lg-6">
-						<ul class="list-unstyled">
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-						</ul>
-					</div>
-					<!-- /.col-lg-6 -->
-					<div class="col-lg-6">
-						<ul class="list-unstyled">
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-							<li><a href="#">Category Name</a>
-							</li>
-						</ul>
-					</div>
-					<!-- /.col-lg-6 -->
+				<div class="col-md-2">
+					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
 				</div>
-				<!-- /.row -->
 			</div>
+			<hr>
+		</div>
 
-			<!-- Side Widget Well -->
-			<div class="well">
-				<h4>Side Widget Well</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+		<div class="col-md-4">
+
+			<!--Blog Post -->
+			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
+			<br>
+			<h2>
+				<a href="#">Blog Post Title</a>
+			</h2>
+			<div class="row">
+				<div class="col-md-10">
+					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
+				</div>
+
+				<div class="col-md-2">
+					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
+				</div>
 			</div>
+			<hr>
+		</div>
 
+		<div class="col-md-4">
+
+			<!--Blog Post -->
+			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
+			<br>
+			<h2>
+				<a href="#">Blog Post Title</a>
+			</h2>
+			<div class="row">
+				<div class="col-md-10">
+					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
+				</div>
+
+				<div class="col-md-2">
+					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
+				</div>
+			</div>
+			<hr>
 		</div>
 
 	</div>
 	<!-- /.row -->
+
+	<!-- Pager -->
+	<ul class="pager">
+		<li class="previous">
+			<a href="#">&larr; Older</a>
+		</li>
+		<li class="next">
+			<a href="#">Newer &rarr;</a>
+		</li>
+	</ul>
+
 </div>
 <!-- /.container -->
