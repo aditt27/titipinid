c3dd09efdcfd8fb8cfa2fe6854cdc2075ce8d6f4
aditt27 <adittbudi@gmail.com>
2018-05-14 20:49:58 +0700

    news small change

--- application/views/isinews.php
+++ application/views/isinews.php
@@ -9,87 +9,37 @@
 				<a href="#">Hot Post Title</a>
 			</h2>
 			<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author</p>
-			<p> August 28, 2013 | <span class="glyphicon glyphicon-eye-open"></span> 0</p>
+			<p> August 28, 2013 | <a style="background: black; border-radius: 4px; color: white; padding-left: 4px; padding-right: 4px; font-family: Open Sans; width: fit-content; font-size: 12px;">teknologi</a></p>
 		</div>
 	</div>
 
 	<hr>
 
 	<div class="row">
-
-		<!-- Blog Entries Column -->
 		<div class="col-md-4">
 
 			<!--Blog Post -->
 			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
-			<br>
-			<h2>
-				<a>Blog Post Title</a>
-			</h2>
-			<div class="row">
-				<div class="col-md-10">
-					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
-				</div>
-
-				<div class="col-md-2">
-					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
-				</div>
+			<div style="background: #f5f5f5; padding: 16px">
+				<h2>
+					<a href="#">Blog Post Title</a>
+				</h2>
+				<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
+				<a style="background: black; border-radius: 4px; color: white; padding-left: 4px; padding-right: 4px; font-family: Open Sans; width: fit-content; font-size: 12px;">teknologi</a>
 			</div>
-			<hr>
-		</div>
 
-		<div class="col-md-4">
-
-			<!--Blog Post -->
-			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
-			<br>
-			<h2>
-				<a href="#">Blog Post Title</a>
-			</h2>
-			<div class="row">
-				<div class="col-md-10">
-					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
-				</div>
-
-				<div class="col-md-2">
-					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
-				</div>
-			</div>
-			<hr>
-		</div>
-
-		<div class="col-md-4">
-
-			<!--Blog Post -->
-			<img class="img-responsive" src="http://www.hanedanrpg.com/photos/hanedanrpg/27/134596.jpg" alt="">
-			<br>
-			<h2>
-				<a href="#">Blog Post Title</a>
-			</h2>
-			<div class="row">
-				<div class="col-md-10">
-					<p style="width: fit-content; padding: 2px; margin-top: 2px; font-family: Berlin Sans FB">Author - August 28, 2013</p>
-				</div>
-
-				<div class="col-md-2">
-					<p><span class="glyphicon glyphicon-eye-open"></span> 0</p>
-				</div>
-			</div>
-			<hr>
 		</div>
 
 	</div>
 	<!-- /.row -->
 
 	<!-- Pager -->
-	<ul class="pager">
-		<li class="previous">
-			<a href="#">&larr; Older</a>
-		</li>
-		<li class="next">
-			<a href="#">Newer &rarr;</a>
-		</li>
-	</ul>
+	<div class="pull-right">
+		<ul class="pagination">
+			<li><a style="color: black; border-radius: 0px; border-color: darkorange" href="#">&larr;</a></li>
+			<li><a style="color: black; border-radius: 0px; border-color: darkorange" href="#">&rarr;</a></li>
+		</ul>
+	</div>
 
 </div>
 <!-- /.container -->
